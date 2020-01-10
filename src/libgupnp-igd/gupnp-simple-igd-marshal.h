ca08bd21061140f2d98e0875d16b4259704b860a
Alex Baranowski <alex@euro-linux.com>
2020-01-10 03:40:39 +0100

    els-ASAP auto commit file/tag gupnp-igd-0.2.2-1.el7.src.rpm

---
+++ src/libgupnp-igd/gupnp-simple-igd-marshal.h
@@ -0,0 +1,28 @@
+
+#ifndef ___gupnp_simple_igd_marshal_MARSHAL_H__
+#define ___gupnp_simple_igd_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:STRING,STRING,STRING,UINT,STRING,UINT,STRING (gupnp-simple-igd-marshal.list:1) */
+extern void _gupnp_simple_igd_marshal_VOID__STRING_STRING_STRING_UINT_STRING_UINT_STRING (GClosure     *closure,
+                                                                                          GValue       *return_value,
+                                                                                          guint         n_param_values,
+                                                                                          const GValue *param_values,
+                                                                                          gpointer      invocation_hint,
+                                                                                          gpointer      marshal_data);
+
+/* VOID:POINTER,STRING,UINT,STRING,UINT,STRING (gupnp-simple-igd-marshal.list:2) */
+extern void _gupnp_simple_igd_marshal_VOID__POINTER_STRING_UINT_STRING_UINT_STRING (GClosure     *closure,
+                                                                                    GValue       *return_value,
+                                                                                    guint         n_param_values,
+                                                                                    const GValue *param_values,
+                                                                                    gpointer      invocation_hint,
+                                                                                    gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___gupnp_simple_igd_marshal_MARSHAL_H__ */
+
